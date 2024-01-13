60529ffef5dec9baabbe27efbe4b384a68259439
DomiJavier <domi95@hotmail.es>
2024-01-13 14:39:19 +0100

    Actualización v3.0

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bcrypt = require("bcrypt")
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connection = require('./db');
 //const { subYears } = require('date-fns');
 const app = express();
+const salt = 10;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -11,18 +13,50 @@ const meses=[1,2,3,4,5,6,7,8,9,10,11,12];
 const mes1=[12,1,2,3,4,5,6,7,8,9,10,11];
 app.post('/login', (req, res) => {
   const { usuario, password } = req.body;
-  const query = `SELECT * FROM usuarios WHERE usuario='${usuario}' AND pass='${password}'`;
-
+  const query = `SELECT * FROM usuarios WHERE usuario='${usuario}'`;
+  console.log(`usuario`);
   connection.query(query, (error, results) => {
     if (error) throw error;
     if (results.length > 0) {
-      console.log(`Usuario = ${usuario} se ha logueado`);
-      res.json({ success: true, message: 'Login exitoso' });
+      console.log(results[0].pass);
+      bcrypt.compare(password, results[0].pass, function(err, response) {
+        if (err){
+          return res.json("Error");
+          console.log(`ERROR`);
+        }
+        if (response){
+          console.log(`Usuario = ${usuario} se ha logueado`);
+          res.json({ success: true, message: 'Login exitoso' });
+        }
+        else{
+          res.json({ success: false, message: 'Credenciales Incorrectas' });
+        }
+      });
     } else {
       console.log(`Usuario = ${usuario} incorrecto`);
-      res.json({ success: false, message: 'Credenciales incorrectas' });
+      res.json({ success: false, message: 'Credenciales Incorrectas' });
     }
   });
+});
+
+app.post('/recuperar', (req, res) => {
+  const { usuario, password } = req.body;
+  bcrypt.hash(password, salt, (err, hash) => {
+    if (err) {
+      console.log(err)
+    }
+    const query = `UPDATE usuarios SET pass='${hash}' WHERE usuario='${usuario}'`;
+    console.log(`usuario perdida`);
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.log(`Usuario = ${usuario} incorrecto`);
+        res.json({ success: false, message: 'Credenciales incorrectas' });
+      } else {
+        console.log(`Usuario = ${usuario} ha recuperado la contraseña`);
+        res.json({ success: true, message: 'Ha recuperado la contraseña' });
+      }  
+  });
+});
 });
 app.get('/sintomas', (req,res) => {
   const query = `SELECT * FROM sintomas`;
@@ -39,11 +73,9 @@ app.post('/crisis', (req, res) => {
   const { duracion, sintomas, usuario } = req.body;
   const fechaC = new Date();
   const horas = fechaC.getHours().toString().padStart(2, '0');
-
-  //fechaC.setMonth(fechaC.getMonth()+1);
   const mes = fechaC.getMonth();
   const fecha = `${fechaC.getFullYear()}-${meses[mes]}-${fechaC.getUTCDate()}`;
-  console.log(fecha)
+  //console.log(fecha)
   connection.beginTransaction((err) => {
     if (err) {
       console.error('Error al iniciar la transacción:', err);
@@ -89,21 +121,18 @@ app.post('/crisis', (req, res) => {
 app.post('/SintomasG', (req, res) => {
   const { usuario } = req.body;
   const fechaC1 = new Date();
-  //fechaC1.setMonth(fechaC1.getMonth()+1);
   const mes = fechaC1.getMonth();
   const fecha1 = `${fechaC1.getFullYear()}-${meses[mes]}-${fechaC1.getUTCDate()}`;
   const fechaC2 = new Date();
   const fecha2 = `${fechaC2.getFullYear()}-${mes1[mes]}-${fechaC2.getUTCDate()}`;
   const query = `SELECT sintomas.sintoma, sintomas.color, count(sintomatologia.sintoma) AS contador FROM crisis INNER JOIN sintomatologia ON crisis.id=sintomatologia.crisis INNER JOIN sintomas ON sintomatologia.sintoma=sintomas.id WHERE crisis.usuario='${usuario}' AND crisis.fecha >= '${fecha2}' AND crisis.fecha <= '${fecha1}' GROUP BY sintomatologia.sintoma;`;
-  console.log(query);
+  //console.log(query);
   connection.query(query, (error, results) => {
     if (error) throw error;
     if (results.length > 0) {
-      //console.log(`Completo`);
       res.json(results);
     } else {
-      //console.log(`Vacío`);
-      data={'name': 'vacío', 'population': 1, 'color': black}
+      data={'name': 'vacío', 'population': 1, 'color': 'black'}
       res.json(data);
     }
   });
@@ -111,41 +140,35 @@ app.post('/SintomasG', (req, res) => {
 app.post('/Horario', (req, res) => {
   const { usuario } = req.body;
   const fechaC = new Date();
-  //fechaC1.setMonth(fechaC1.getMonth()+1);
   const mes = fechaC.getMonth();
   const fecha1 = `${fechaC.getFullYear()}-${meses[mes]}-${fechaC.getUTCDate()}`;
   const fecha2 = `${fechaC.getFullYear()}-${mes1[mes]}-${fechaC.getUTCDate()}`;
   const query = `SELECT CONCAT(horario,':00') AS 'horario', count(horario) as cuenta FROM crisis WHERE usuario='${usuario}' AND fecha >= '${fecha2}' AND fecha <= '${fecha1}' GROUP BY horario;`;
-  console.log(query);
+  //console.log(query);
   connection.query(query, (error, results) => {
-    if (error) throw error;
     if (results.length > 0) {
-      //console.log(`Completo`);
+      if (error) throw error;
       res.json(results);
     } else {
-      //console.log(`Vacío`);
-      data={'name': 'vacío', 'population': 1, 'color': black}
+      data={'name': 'vacío', 'population': 1, 'color': 'black'}
       res.json(data);
     }
   });
 });
 app.post('/Mensual', (req, res) => {
-  //console.log(`Mensual`);
   const { usuario } = req.body;
   const fechaC = new Date();
   const mes = fechaC.getMonth();
   const fecha1 = `${fechaC.getFullYear()}-${meses[mes]}-${fechaC.getUTCDate()}`;
   const fecha2 = `${fechaC.getFullYear()}-01-01`;
   const query = `SELECT MONTH(fecha) AS mes, count(id) AS id FROM crisis WHERE usuario=${usuario} AND fecha >= '${fecha2}' AND fecha <= '${fecha1}' GROUP BY MONTH(fecha) ORDER BY MONTH(fecha);`;
-  console.log(query);
+  //console.log(query);
   connection.query(query, (error, results) => {
     if (error) throw error;
     if (results.length > 0) {
-      //console.log(`Completo`);
       res.json(results);
     } else {
-      //console.log(`Vacío`);
-      data={'name': 'vacío', 'population': 1, 'color': black}
+      data={'name': 'vacío', 'population': 1, 'color': 'black'}
       res.json(data);
     }
   });
@@ -166,34 +189,45 @@ app.post('/Medicacion', (req, res) => {
     }
   });
 });
+app.post('/HistoricoMedicacion', (req, res) => {
+  const { usuario } = req.body;
+  const fechaC = new Date();
+  const mes = fechaC.getMonth();
+  const fecha = `${fechaC.getFullYear()}-${meses[mes]}-${fechaC.getUTCDate()}`;
+  const query = `SELECT medicacion, CONCAT(hora,':00') AS 'hora', DATE_FORMAT(fecha, '%d-%m-%Y') as fecha FROM medicacion WHERE usuario='${usuario}' AND fecha < '${fecha}'`;
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      data={'medicacion': 'vacío', 'hora': 0, 'fecha': ''}
+      res.json(data);
+    }
+  });
+});
 app.post('/Notificaciones', (req, res) => {
   const { usuario } = req.body;
   const fechaC = new Date();
   const mes = fechaC.getMonth();
   const fecha1 = `${fechaC.getFullYear()}-${meses[mes]}-${fechaC.getUTCDate()}`;
   const query = `SELECT medicacion, hora, fecha FROM medicacion WHERE usuario='${usuario}' AND fecha >= '${fecha1}'`;
-  console.log(query);
+  //console.log(query);
   connection.query(query, (error, results) => {
     if (error) throw error;
     if (results.length > 0) {
-      //console.log(`Completo`);
       res.json(results);
     } else {
       data={'medicacion': 'vacío', 'hora': 0, 'fecha': ''}
-      //console.log(data);
       res.json(data);
     }
   });
 });
 app.post('/NuevaMedicacion', (req, res) => {
   const { usuario, hora, medicacion, fecha } = req.body;
-  console.log(fecha);
   const mes = fecha.getMonth();
   const fechaF = `${fecha.getFullYear()}-${meses[mes]}-${fecha.getUTCDate()}`;
-
-  console.log(fechaF);
   const query = `INSERT INTO medicacion VALUES(0,'${medicacion}','${hora}','${usuario}','${fechaF}')`;
-  console.log(query);
+  //console.log(query);
   connection.query(query, (error, results) => {
     if (error) throw error;
     if (results.length > 0) {
